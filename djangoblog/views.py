e0b788c3d2f0a4942646a043e115ff860f4bb3aa
wonsangyoo <akarws@gmail.com>
2021-07-26 01:00:50 -0400

    Initial Commit

---
+++ djangoblog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse('homepage')
+    return render(request, 'homepage.html')
+
+def about(request):
+    # return HttpResponse('about')
+    return render(request, 'about.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
