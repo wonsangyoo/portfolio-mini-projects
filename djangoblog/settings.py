ee77305f10eabe37d2430b6700d0fea1e0f420d9
wonsangyoo <akarws@gmail.com>
2021-07-26 01:26:26 -0400

    Updated Settings

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fz55%di5p9c&rgrvb4zomw#^s((abhloiehra5-#7fqqa8@al%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-fz55%di5p9c&rgrvb4zomw#^s((abhloiehra5-#7fqqa8@al%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://wonsangyoo.herokuapp.com']
 
 
 # Application definition
